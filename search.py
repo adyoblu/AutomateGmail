0c0c282affcc3e6802ad8b18be0ba1da8fa67258
Adrian <adyoblu2000@yahoo.com>
2023-06-21 19:29:06 +0300

    first day

--- search.py
+++ search.py
@@ -1,12 +1,13 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, jsonify, render_template
 import pickle
 import os.path
 from apiclient import errors
 import email
-import subprocess
-import webbrowser
 from datetime import datetime
 import pytz
+import sys
+import signal
+import os
 from email.mime.text import MIMEText
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -17,18 +18,14 @@ import base64
 SCOPES = ['https://mail.google.com/']
 
 app = Flask(__name__, template_folder='templates')
-app.jinja_env.trim_blocks = True
-app.jinja_env.lstrip_blocks = True
 
-search_string = None
+search_string = sys.argv[1]
 
-def display_messages_today():
-    # Implementați codul pentru a afișa mesajele din inbox de astăzi
-    pass
-
-def create_message_backup():
-    # Implementați codul pentru a crea un backup pentru mesaje
-    pass
+@app.route('/close-server', methods=['POST'])
+def close_server():
+    os.kill(os.getpid(), signal.SIGINT)
+    
+    return jsonify
 
 @app.route('/')
 def display_message():
@@ -117,7 +114,6 @@ def get_message(service, user_id, msg_id):
 #return va fi <googleapiclient.discovery.Resource at 0x284631d0790>
 def get_service():
     creds = None
-
     # token.pickle salveaza accesul user-ului the user access, refresh_token si este generat automat 
     # cand e autorizata aplicatia de user
     if os.path.exists('token.pickle'):
@@ -138,53 +134,5 @@ def get_service():
     service = build('gmail', 'v1', credentials=creds)
     return service
 
-
-def display_menu():
-    print("Meniu:")
-    print("1. Afiseaza mesajele de astazi")
-    print("2. Afiseaza mesajele in functie de subiect anume")
-    print("3. Fa backup mesaje")
-    print("0. Ieșire")
-
-def handle_option(option):
-    if option == 1:
-        os.system('cls')
-        print("Ați selectat Opțiunea 1")
-        display_messages_today()
-        # Adăugați aici codul corespunzător pentru Opțiunea 1
-    elif option == 2:
-        os.system('cls')
-        print("Ați selectat Opțiunea 2")
-        search_string = input("Introduceți un string de cautat in subiecte: ")
-        print("Ați introdus:", search_string)
-        webbrowser.open("http://127.0.0.1:5000", new=0)
-        server_process = subprocess.run(['python', 'search.py', search_string])
-        
-        
-        # Adăugați aici codul corespunzător pentru Opțiunea 2
-    elif option == 3:
-        os.system('cls')
-        print("Ați selectat Opțiunea 3")
-        # Adăugați aici codul corespunzător pentru Opțiunea 3
-    elif option == 0:
-        os.system('cls')
-        print("La revedere!")
-        return False
-    else:
-        os.system('cls')
-        print("Opțiune invalidă. Vă rugăm să selectați o opțiune validă.")
-    return True
-
-def main():
-    while True:
-        os.system('cls')
-        display_menu()
-        option = int(input("Introduceți opțiunea: "))
-        if not handle_option(option):
-            break
-
-def run_local_server():
-    subprocess.run(['flask', 'run'], check=True)
-#display_message()
 if __name__ == '__main__':
-    main()
+    app.run(debug=True)
